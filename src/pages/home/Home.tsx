c596f60f794b25bb6aa121b86f6d4ab89f52b0eb
David Obodo <obododavid5@gmail.com>
2021-09-08 10:57:27 +0100

    feature: display banks

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
-    return <div>this is the home</div>;
+    const [banks, setBanks] = useState<never[]>([]);
+
+    useEffect(() => {
+        const fetchBanks = async () => {
+            try {
+                const res = await axios.get("https://api.paystack.co/bank");
+                setBanks(res.data.data);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        fetchBanks();
+    }, []);
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="bank">Select bank</label>
+                <select name="bank" id="bank">
+                    {banks.map((bank) => {
+                        const { name } = bank;
+                        return <option value="">{name}</option>;
+                    })}
+                </select>
+            </div>
+        </form>
+    );
 };
 
 export default Home;
