8f8e482eb5325df9634cb533b4660e680c98f4da
David Obodo <obododavid5@gmail.com>
2021-09-13 12:53:15 +0100

    feature: create history table

--- src/pages/history/History.tsx
+++ src/pages/history/History.tsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { ITransaction } from "../../interfaces";
 
+import "./History.scss";
+
 const History = () => {
     const [transferHistory, setTransferHistory] = useState([]);
     useEffect(() => {
@@ -22,31 +24,32 @@ const History = () => {
         fetchAllTransfers();
     }, []);
     return (
-        <div>
-            History page
-            <div>
-                <div>Amount</div>
-                <div>Bank</div>
-                <div>Recipient</div>
-                <div>Date</div>
+        <div className="history-page">
+            <div className="history-page__table">
+                <div className="history-page__table__header">
+                    <div>S/N</div>
+                    <div>Amount</div>
+                    <div>Bank</div>
+                    <div>Recipient</div>
+                    <div>Date</div>
+                </div>
+                <div className="history-page__table__body">
+                    {transferHistory.map((item: ITransaction, index) => {
+                        const { amount, id, recipient, createdAt } = item;
+                        return (
+                            <div key={id} className="history-page__table__body__row">
+                                <div>{index + 1}</div>
+                                <div>{amount}</div>
+                                <div>{recipient.details.bank_name}</div>
+                                <div>{recipient.name}</div>
+                                <div>{createdAt}</div>
+                            </div>
+                        );
+                    })}
+                </div>
             </div>
-            {transferHistory.map((item: ITransaction) => {
-                const { amount, id, recipient, createdAt } = item;
-                return (
-                    <div key={id}>
-                        <div>{amount}</div>
-                        <div>{recipient.details.bank_name}</div>
-                        <div>{recipient.name}</div>
-                        <div>{createdAt}</div>
-                    </div>
-                );
-            })}
         </div>
     );
 };
 
 export default History;
-
-/*
-modal data
-*/
